f5ef8e7cbc6f5fc532fa6ad334f125f3c2627899
cdetviler <charlie.detviler@gmail.com>
2024-09-22 23:42:14 -0500

    (feat) icon: implements icon component

---
+++ components/Icon/Icon.type.ts
@@ -0,0 +1,37 @@
+type iconColor = 'primary' | 'secondary' | 'tertiary' | 'neutral-light' | 'neutral-dark' | 'inherit';
+type iconSize = 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'inherit';
+type iconType = 
+'CircleAlert' |
+'Menu' | 
+'ArrowLeft' | 
+'ArrowRight' | 
+'ArrowUp' | 
+'ArrowDown' |
+'ArrowUpRight' |
+'ArrowUpLeft' |
+'ArrowDownRight' |
+'ArrowDownLeft' |
+'ArrowBigLeftDash' |
+'ArrowBigRightDash' |
+'ArrowBigDownDash' |
+'ArrowBigUpDash' |
+'ChevronRight' |
+'ChevronLeft' |
+'ChevronDown' |
+'ChevronUp' |
+'Maximize' |
+'Minimize' |
+'Twitter' |
+'Facebook' |
+'Linkedin' |
+'Github' |
+'RefreshCw'
+
+interface IconProps {
+    color?: iconColor;
+    size?: iconSize;
+    type: iconType;
+    className?: string;
+}
+
+export type { IconProps, iconColor, iconSize, iconType };
